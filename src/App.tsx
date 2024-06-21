1f879f9eaa8365a9a11b70d8a233e5c582f43494
Emrah Yurtlu <emrahyurtlu@gmail.com>
2024-06-21 22:12:10 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Counter: {count}</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)} style={{border: "1px solid red"}}>
+          + Increment
+        </button>
+        <button onClick={() => setCount((count) => count - 1)} style={{marginLeft: '10px', border: "1px solid red"}}>
+          - Decrement
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
